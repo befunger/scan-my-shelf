7da4308f2001408e419e72bd49a243347e425604
befunger <alexander.d.willemsen@gmail.com>
2023-09-26 11:09:21 -0700

    updated credential handling

--- googlebooks.py
+++ googlebooks.py
@@ -3,6 +3,12 @@
 # For making HTTP requests to the Google Books API
 import requests
 
+with open("C:/Projects/scan-my-shelf/API/GoogleBooks.txt", 'r') as f:
+    GOOGLE_BOOKS_API_KEY = f.read().strip('key=')
+
+GOOGLE_BOOKS_API_BASE_URL = "https://www.googleapis.com/books/v1/volumes"
+
+# DEPRECATED FUNCTION
 def get_credentials():
     '''Retrieves credentials for Google Books API'''
     with open("../API/GoogleBooks.txt", 'r') as f:
@@ -13,14 +19,13 @@ def get_credentials():
 
 def search_books(query):
     '''Search for a book query using the Google Books API'''
-    # Get credentials for API usage
-    credentials = get_credentials()
     params = {
         'q': query,
-        'key': credentials['key']
+        'key': GOOGLE_BOOKS_API_KEY,
+        'maxResults': 10
     }
 
-    response = requests.get(credentials['URL'], params=params)
+    response = requests.get(GOOGLE_BOOKS_API_BASE_URL, params=params)
 
     if response.status_code == 200:
         data = response.json()
