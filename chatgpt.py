fff398ab97f767d5ed1d8882051ab5dd88791244
befunger <alexander.d.willemsen@gmail.com>
2023-09-26 11:08:54 -0700

    chatgpt utility

---
+++ chatgpt.py
@@ -0,0 +1,39 @@
+'''Utilities for using the ChatGPT API'''
+import openai   # For ChatGPT access
+import json     # For json handling
+
+# TODO: Change this to import from openai.txt
+openai.api_key = "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"
+
+def format_prompt(extracted_text):
+    '''Formats a ready prompt from the extracted text'''
+    GPT_GET_BOOKS_PROMPT = f'''Extract all book titles from the following extracted text from an image. Only include real book titles.
+    
+    Extracted text: {extracted_text}
+
+    Format: {{"books": ['book_1', 'book_2']}}
+    '''
+
+    return GPT_GET_BOOKS_PROMPT
+
+def get_gpt_response(extracted_text: str,
+                     model_name: str="gpt-3.5-turbo"
+                     ) -> str:
+    '''Get responses from an OpenAI LLM model for given queries'''
+
+    gpt_input = format_prompt(extracted_text)
+
+    completion = openai.ChatCompletion.create(
+        model = model_name,
+        messages=[
+            {"role": "system", "content": "Output only valid JSON"},
+            {"role": "user", "content": gpt_input}
+        ],
+        temperature = 0.7
+    )
+
+    text = completion.choices[0].message.content
+    # print(text)
+    parsed = json.loads(text)
+
+    return parsed
