86e3e33aeef4b1bd254a3967310eabd61ca6a5a5
befunger <alexander.d.willemsen@gmail.com>
2023-10-04 08:37:04 -0700

    testing moved to separate file

--- openlibrary.py
+++ openlibrary.py
@@ -28,27 +28,3 @@ def search_query(query):
     '''Search book with query'''
     params = {"q": query}
     return search_books(params)
-
-
-# Book title and author
-query = "Dragon Thunder Kalla"
-title = "Dragon Thunder"
-author = "Kalla"
-
-# Search for the book
-# books = search_title_and_author(title, author)
-# books = search_title(title)
-# books = search_query(query)
-
-# Display book information
-print(len(books), "books found.")
-if books:
-    book = books[0]
-    print("Title:", book.get('title', 'N/A'))
-    print("Author:", book.get('author_name', ['N/A'])[0])
-    print("Publish Year:", book.get('first_publish_year', 'N/A'))
-    print("Number of Pages:", book.get('number_of_pages', 'N/A'))
-    # print("Publishers:", book.get('publisher', ['N/A'])[0])
-    # print("Subjects:", book.get('subject', []))
-else:
-    print("Book not found.")
