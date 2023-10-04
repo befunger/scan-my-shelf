d5231bbc06335cd138b5330d4bca896739b5b345
befunger <alexander.d.willemsen@gmail.com>
2023-10-04 07:34:28 -0700

    encoding specified + cleanup

--- chatgpt.py
+++ chatgpt.py
@@ -2,42 +2,35 @@
 import json     # For json handling
 import openai   # For ChatGPT access
 
-with open("C:/Projects/scan-my-shelf/API/openai.txt", 'r') as f:
+with open("C:/Projects/scan-my-shelf/API/openai.txt", 'r', encoding="utf-8") as f:
     openai.api_key = f.read()
-
-def format_prompt_1(extracted_text):
-    '''Formats a ready prompt from the extracted text'''
-    GPT_GET_BOOKS_PROMPT = f'''Extract the book title from the following extracted text from an image. The text may contain typos as well as garbage. Only include real book titles. Include only 1 title.
-    
-    Extracted text: {extracted_text}
-
-    Format: {{"books": ['book']}}
-    '''
-
-    return GPT_GET_BOOKS_PROMPT
 
 def format_single_prompt(extracted_text):
     '''Formats prompt for detecting single book'''
 
-    GPT_GET_BOOKS_PROMPT = f'''Extract the book title from the following extracted text from an image. The text may contain typos as well as garbage. Only include real book titles. Include the author of the book.
+    gpt_get_books_prompt = f'''Extract the book title from the following extracted text from an image. The text may contain typos as well as garbage. Only include real book titles. Include the author of the book.
     
     Extracted text: {extracted_text}
 
     Format: {{"book": ['book'], "author": ['author']}}
     '''
-    return GPT_GET_BOOKS_PROMPT
+    return gpt_get_books_prompt
 
 def format_multi_prompt(extracted_text):
     '''Formats prompt for detecting multiple books'''
 
-    GPT_GET_BOOKS_PROMPT = f'''Extract the book titles from the following extracted text from an image. The text may contain typos as well as garbage. Only include real book titles. Include the authors of the books.
+    gpt_get_books_prompt = f'''Extract the book titles from the following extracted text from an image. The text may contain typos as well as garbage. Only include real book titles. Include the authors of the books.
         
     Extracted text: {extracted_text}
 
     Format: {{"book": ['book_1', 'book_2'], "author": ['author_1', 'author_2']}}
     '''
-    return GPT_GET_BOOKS_PROMPT
+    return gpt_get_books_prompt
 
+def check_book(text: str):
+    '''Verifies whether a title-author pair is a legitimate book.'''
+    # TODO: Replace with a prompt to ChatGPT. Do Prompt Engineering.
+    return True
 
 def get_titles_from_text(extracted_text: str,
                         multi_detect: bool=False,
@@ -60,7 +53,6 @@ def get_titles_from_text(extracted_text: str,
     )
 
     text = completion.choices[0].message.content
-    # print(text)
     parsed = json.loads(text)
 
     return parsed
