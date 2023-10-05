fda4e9c2a6c17d18b89c205f4bde1d993e11ea9f
befunger <alexander.d.willemsen@gmail.com>
2023-10-04 18:34:33 -0700

    test file for pinging api

---
+++ test_api.py
@@ -0,0 +1,27 @@
+import requests
+
+# Endpoint URL
+API_URL = 'http://127.0.0.1:8000/recognize_books'  # Change this to your actual API URL
+
+def test_api_with_image(image_path):
+    '''Tests the API with a given image'''
+    # # Load the image file
+    # image_file = open(image_path, 'rb')
+
+    # # Make a POST request to the API endpoint with the image
+    # response = requests.post(API_URL, files={'image': image_file})
+
+    print("Sending request. This may take a while.")
+    # For now, no variable image parameter
+    response = requests.post(API_URL, files={})
+
+    # Print the response from the API
+    print("Response:", response.status_code)
+    print("JSON response:", response.json())
+
+if __name__ == '__main__':
+    # Image path for test image
+    image_path = "C:\\Projects\\scan-my-shelf\\images\\shelf3.jpg"
+
+    # Call the test function
+    test_api_with_image(image_path)
